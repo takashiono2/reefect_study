0a20a1c99743663321871925e642f8fe17cd5799
takashiono2 <takashiono710@gmail.com>
2024-08-10 22:45:13 +0900

    react-hooks-modal

--- react-hooks-modal/src/App.jsx
+++ react-hooks-modal/src/App.jsx
@@ -1,15 +1,11 @@
 import useModal from './hooks/useModal';
 
 function App() {
-  const { Modal, openModal, closeModal } = useModal();
+  const { Modal, openModal, closeModal, show } = useModal();
 
   return (
     <div style={{ margin: '2em' }}>
-      <h1>React Hooksでモーダルウィンドウを作成</h1>
-      <div>
-        <button onClick={openModal}>Open</button>
-      </div>
-      <Modal>
+      <Modal show={show}>
         <div style={{
           backgroundColor: 'white',
           width: '300px',
@@ -21,6 +17,14 @@ function App() {
           <button onClick={closeModal}>close</button>
         </div>
       </Modal>
+      <h1>React Hooksでモーダルウィンドウを作成</h1>
+      <div>
+        <button onClick={openModal}>Open</button>
+      </div>
+
+      <div style={{ height: '2000px', backgroundColor: '#ddd' }} id="main">
+        メインコンテンツ
+      </div>
     </div>
   );
 }
--- react-hooks-modal/src/hooks/useModal.jsx
+++ react-hooks-modal/src/hooks/useModal.jsx
@@ -1,4 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
 
 const useModal = () => {
   const [show, setShow] = useState(false);
@@ -11,9 +17,27 @@ const useModal = () => {
     setShow(false);
   }
 
+  // eslint-disable-next-line react/prop-types
   const Modal = ({ children }) => {
+    const contentRef = useRef(null);
+
+    useEffect(() => {
+      if (contentRef.current === null) return;
+
+      if (show) {
+        disableBodyScroll(contentRef.current);
+      } else {
+        enableBodyScroll(contentRef.current);
+      }
+
+      return () => {
+        clearAllBodyScrollLocks();
+      };
+    }, [contentRef]);
+
     if (!show) return null;
-    return (
+
+    return createPortal(
       <div style={{
         position: "fixed",
         top: 0,
@@ -33,12 +57,15 @@ const useModal = () => {
           opacity: 0.5,
           backgroundColor: "gray",
         }}></div>
-        <div style={{ position: 'relative' }}>
-          {children}
-        </div></div>
+          <div style={{ position: 'relative' }} ref={contentRef}>
+            {children}
+          </div>
+        </div>
+      ,
+      document.getElementById('root')
     )
   };
-  return { Modal, openModal, closeModal };
+  return { Modal, openModal, closeModal, show };
 };
 
 export default useModal;
