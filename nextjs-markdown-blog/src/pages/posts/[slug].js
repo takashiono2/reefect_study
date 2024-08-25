8e326a354d53e8cb4e9ac55435a1171860521a33
takashiono2 <takashiono710@gmail.com>
2024-08-25 19:17:29 +0900

    getstaticpaths

--- nextjs-markdown-blog/src/pages/posts/[slug].js
+++ nextjs-markdown-blog/src/pages/posts/[slug].js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+
+export async function getStaticProps() {
+  return { props: { post: '' } };
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync('posts');
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(/\.md$/, ''),
+    },
+  }));
+  console.log('paths:', paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
 const Post = () => {
   return <div>コンテンツ</div>;
 };
